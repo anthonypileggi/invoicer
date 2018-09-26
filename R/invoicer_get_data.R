bfcf89f3480f293a2715e74ad7a61d2dbdbbc860
anthonypileggi <anthony@ifixit.com>
2018-09-26 16:41:09 -0400

    silence googlesheets

--- R/invoicer_get_data.R
+++ R/invoicer_get_data.R
@@ -8,7 +8,7 @@ invoicer_get_data <- function(key = Sys.getenv("INVOICER_GS_KEY")) {
 
   # TODO: fix authentication pipeline
   googlesheets::gs_auth()
-  gs <- googlesheets::gs_key(key)
+  gs <- googlesheets::gs_key(key, verbose = FALSE)
 
   # prepare client data
   clients <- invoicer_get_clients(gs = gs)
@@ -40,11 +40,11 @@ invoicer_get_company <- function(key = Sys.getenv("INVOICER_GS_KEY"), gs = NULL)
   # TODO: fix authentication pipeline
   if (is.null(gs)) {
     googlesheets::gs_auth()
-    gs <- googlesheets::gs_key(key)
+    gs <- googlesheets::gs_key(key, verbose = FALSE)
   }
 
   # prepare company data
-  x <- googlesheets::gs_read(gs, ws = "company")
+  x <- googlesheets::gs_read(gs, ws = "company", verbose = FALSE, col_types = readr::cols())
   dplyr::mutate(
     x,
     html = paste0(client, htmltools::tags$br(), address, htmltools::tags$br(), city, ", ", state, " ", zip_code)
@@ -61,11 +61,11 @@ invoicer_get_clients <- function(key = Sys.getenv("INVOICER_GS_KEY"), gs = NULL)
   # TODO: fix authentication pipeline
   if (is.null(gs)) {
     googlesheets::gs_auth()
-    gs <- googlesheets::gs_key(key)
+    gs <- googlesheets::gs_key(key, verbose = FALSE)
   }
 
   # prepare client data
-  clients <- googlesheets::gs_read(gs, ws = "clients")
+  clients <- googlesheets::gs_read(gs, ws = "clients", verbose = FALSE, col_types = readr::cols())
   dplyr::mutate(
     clients,
     html = paste0(client, htmltools::tags$br(), address, htmltools::tags$br(), city, ", ", state, " ", zip_code)
@@ -82,11 +82,11 @@ invoicer_get_worklog <- function(key = Sys.getenv("INVOICER_GS_KEY"), gs = NULL)
   # TODO: fix authentication pipeline
   if (is.null(gs)) {
     googlesheets::gs_auth()
-    gs <- googlesheets::gs_key(key)
+    gs <- googlesheets::gs_key(key, verbose = FALSE)
   }
 
   # prepare worklog data
-  worklog <- googlesheets::gs_read(gs, ws = "worklog")
+  worklog <- googlesheets::gs_read(gs, ws = "worklog", verbose = FALSE, col_types = readr::cols())
   dplyr::mutate_at(worklog, "date", as.Date, format = c("%m/%d/%Y"))
 }
 
@@ -100,11 +100,11 @@ invoicer_get_invoices <- function(key = Sys.getenv("INVOICER_GS_KEY"), gs = NULL
   # TODO: fix authentication pipeline
   if (is.null(gs)) {
     googlesheets::gs_auth()
-    gs <- googlesheets::gs_key(key)
+    gs <- googlesheets::gs_key(key, verbose = FALSE)
   }
 
   # prepare invoice data
-  invoices <- googlesheets::gs_read(gs, ws = "invoices")
+  invoices <- googlesheets::gs_read(gs, ws = "invoices", verbose = FALSE, col_types = readr::cols())
   invoices <- dplyr::mutate_at(invoices, c("start_date", "end_date"), as.Date, format = c("%m/%d/%Y"))
   dplyr::filter(invoices, !is.na(id))
 }
