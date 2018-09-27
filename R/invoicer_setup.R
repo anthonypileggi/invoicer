b39ffcc63dde00ac1036d5b41ce13045b50fc5c3
anthonypileggi <anthony@ifixit.com>
2018-09-27 15:49:13 -0400

    invoicer_setup: add company ws, include 'email' arg, don't create emails/payments yet

--- R/invoicer_setup.R
+++ R/invoicer_setup.R
@@ -2,9 +2,19 @@
 #' Run this function to create the googlesheets template
 #' @param name Company name
 #' @export
-invoicer_setup <- function(name = "AVP Consulting") {
+invoicer_setup <- function(name = "AVP Consulting", email = "apileggi20@gmail.com") {
 
   # create sample data
+  sample_company <-
+    tibble::tibble(
+      name = name,
+      address = "",
+      city = "",
+      start = "",
+      zip_code = "",
+      email = email,
+      logo = ""
+    )
   sample_clients <-
     tibble::tibble(
       client = c("Client A", "Client B"),
@@ -12,17 +22,27 @@ invoicer_setup <- function(name = "AVP Consulting") {
       city = c("City A", "City B"),
       state = c("CA", "PA"),
       zip_code = c(12345, 67890),
-      email = c("clienta@gmail.com", "clientb@gmail.com"),
+      email = email,
       rate = c(50, 75)
     )
   sample_worklog <-
     tibble::tibble(
-      date = c("9/17/2018", "9/18/2018"),
+      date = Sys.Date(),
       client = c("Client A", "Client A"),
       project = c("Project 1", "Project 2"),
       details = c("", ""),
-      links = c("http://github.com/anthonypileggi/invoicer/issues/1", "http://github.com/anthonypileggi/invoicer/issues/2"),
+      links = c("https://github.com/anthonypileggi/invoicer/issues/1", "https://github.com/anthonypileggi/invoicer/issues/1"),
       hours = c(5, 10)
+    )
+  sample_invoice <-
+    tibble::tibble(
+      id = 0,
+      client = "test",
+      start_date = Sys.Date(),
+      end_date = Sys.Date(),
+      total = 0,
+      drive_id = "xxxx",
+      file = "xxxx.pdf"
     )
 
   # create google sheets
@@ -32,35 +52,38 @@ invoicer_setup <- function(name = "AVP Consulting") {
       title = name,
       ws_title = "worklog",
       input = sample_worklog,
-      col_extent = 6
+      col_extent = ncol(sample_worklog)
       )
+  googlesheets::gs_ws_new(
+    ss,
+    ws_title = "company",
+    input = sample_company,
+    col_extent = ncol(sample_company)
+  )
   googlesheets::gs_ws_new(
     ss,
     ws_title = "clients",
     input = sample_clients,
-    col_extent = 6
+    col_extent = ncol(sample_clients)
   )
   googlesheets::gs_ws_new(
     ss,
     ws_title = "invoices",
-    input = c(
-      "id", "client", "start_date", "end_date", "total", "drive_id", "file",
-      0, "test", "9/17/2018", "9/18/2018", 0, "xxxx", "xxx.html"
-      ),
-    col_extent = 7
+    input = sample_invoice,
+    col_extent = ncol(sample_invoice)
   )
-  googlesheets::gs_ws_new(
-    ss,
-    ws_title = "emails",
-    input = c("id", "recipient", "date"),
-    col_extent = 3
-  )
-  googlesheets::gs_ws_new(
-    ss,
-    ws_title = "payments",
-    input = c("id", "date", "total"),
-    col_extent = 3
-  )
+  # googlesheets::gs_ws_new(
+  #   ss,
+  #   ws_title = "emails",
+  #   input = c("id", "recipient", "date"),
+  #   col_extent = 3
+  # )
+  # googlesheets::gs_ws_new(
+  #   ss,
+  #   ws_title = "payments",
+  #   input = c("id", "date", "total"),
+  #   col_extent = 3
+  # )
 
   Sys.setenv(INVOICER_GS_KEY = ss$sheet_key)
 }
