960c8e81eeae0fc67d2bb82e22a1487710c95e75
anthonypileggi <anthony@ifixit.com>
2018-10-26 12:06:07 -0400

    fix typo when setting recipient email addresses for the client

--- R/invoicer_email.R
+++ R/invoicer_email.R
@@ -85,7 +85,7 @@ invoicer_email_send <- function(x, preview = FALSE) {
   blastula::send_email_out(
     email,
     from = me$email,
-    to = stringr::str_trim(stringr::str_split(clients$email, ",")[[1]]),    # parse the comma-separated list of client email addresses
+    to = stringr::str_trim(stringr::str_split(client$email, ",")[[1]]),    # parse the comma-separated list of client email addresses
     cc = me$email,
     subject = paste0("Invoice from ", me$name, " (", format(x$start_date, "%m/%d/%Y"), " - ", format(x$end_date, "%m/%d/%Y"), ")"),
     attachments = pdf_file,
